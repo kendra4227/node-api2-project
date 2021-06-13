ed51c671db2c0ee338551b0faf3f527f4b8b001d
Lakendria <kendra939@gmail.com>
2021-06-12 21:15:34 -0400

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -40,17 +40,16 @@ router.get('/:id',async(req,res)=>{
     }
     
 })
-router.post('/api/posts',async(req,res)=>{
+router.post('/',async(req,res)=>{
     const post = req.body;
 
     try {
       if (!post.title || !post.contents) {
         res
-          .status(404)
+          .status(400)
           .json({ message: "Please provide title and contents for the post" });
       } else {
         const getId = await Posts.insert(post);
-        //const newPost = { ...getId, title: post.title, contents: post.contents };
         const newPost = await Posts.findById(getId);
         res.status(201).json(newPost);
       }
@@ -61,15 +60,72 @@ router.post('/api/posts',async(req,res)=>{
     }
    
 })
-router.delete('/:id',(req,res)=>{
-    
-})
-router.put('/:id',(req,res)=>{
-    
-})
-router.get('/:id/messages',(req,res)=>{
-    
-})
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const deleted = await Posts.findById(id);
+      if (!deleted) {
+        res
+          .status(404)
+          .json({ message: `The post with id ${id} does not exist` });
+      } else {
+        const deletedPost = await Posts.findById(id);
+      res.status(200).json(deletedPost);
+      await Posts.remove(id);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "The post could not be removed" });
+    }
+  });
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const post = req.body;
+  
+    try {
+      if (!post.title || !post.contents) {
+        res
+          .status(400)
+          .json({ message: "Please provide title and contents for the post" });
+      } else {
+        const updatedPost = await db.update(id, post);
+        if (!updatedPost) {
+          res
+            .status(404)
+            .json({ message: `The post with id ${id} does not exist` });
+        } else {
+            const postUpdated = await Posts.findById(id);
+        res.status(200).json(postUpdated);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "The post information could not be modified" });
+    }
+  });
+  router.get("/:postID/comments", async (req, res) => {
+    const { postID } = req.params;
+  
+    try {
+      const getPostID = await Posts.findById(postID);
+      if (!getPostID) {
+        res
+          .status(404)
+          .json({ message: `The post with id ${postID} does not exist` });
+      } else {
+        const comments = await Posts.findPostComments(postID);
+        res.json(comments);
+      }
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "The comments information could not be retrieved" });
+    }
+  });
+  
 
 
 
